7d3c0692fdfc3640bfcc70e8c82063f3c215a684
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-08-13 10:56:28 -0400

    refactor: Clean up example code

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,11 @@ fn main() {
 
     let square_ = change_amplitude(square_wave_table(64), 0.02);
     let sin_ = change_amplitude(sin_wave_table(64), 0.1);
-    let combine_ = combine(sin_, square_);
-
     let test_ = perlin_wave_table(64, 1000000100);
-
-    // println!("{:#?}", test_);
-
+    let combine_ = combine(combine(sin_, square_), test_);
     let mut oscillator = WavetableOscillator::new(44100, combine_);
-
     oscillator.set_frequency(442.0);
-    
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    
     let _result = stream_handle.play_raw(oscillator.convert_samples());
 
 
