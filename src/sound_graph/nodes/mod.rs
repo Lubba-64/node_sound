a5ba3f4b007ffb0b6a3765c30c4c45ea64768476
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-08-16 18:15:37 -0400

    feat: Add triangle wave

--- src/sound_graph/nodes/mod.rs
+++ src/sound_graph/nodes/mod.rs
@@ -2,6 +2,8 @@ mod mix_node;
 use mix_node::mix_node;
 mod sawtooth_node;
 use sawtooth_node::sawtooth_node;
+mod triangle_node;
+use triangle_node::traingle_node;
 mod sounds;
 pub use sounds::*;
 use std::collections::BTreeMap;
@@ -9,7 +11,7 @@ use std::collections::BTreeMap;
 use super::types::NodeDefinitions;
 
 pub fn get_nodes() -> NodeDefinitions {
-    let nodes = [sawtooth_node(), mix_node()];
+    let nodes = [sawtooth_node(), mix_node(), traingle_node()];
     NodeDefinitions(BTreeMap::from_iter(
         nodes.iter().map(|n| (n.name.clone(), n.clone())),
     ))
