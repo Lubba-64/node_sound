5a58990c93307054ae18466026b7d65647b41946
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2024-04-15 18:56:45 -0400

    refactor: Move duration stuff into take duration node

--- src/nodes/duration_node.rs
+++ src/nodes/duration_node.rs
@@ -7,25 +7,26 @@ use crate::sounds::AsGenericSource;
 use egui_node_graph::InputParamKind;
 use rodio::Source;
 use std::collections::HashMap;
-pub fn mix_node() -> SoundNode {
+
+pub fn duration_node() -> SoundNode {
     SoundNode {
-        name: "Mix".to_string(),
+        name: "Take Duration".to_string(),
         inputs: HashMap::from([
+            (
+                "duration".to_string(),
+                InputParameter {
+                    data_type: DataType::Duration,
+                    kind: InputParamKind::ConnectionOrConstant,
+                    name: "duration".to_string(),
+                    value: InputValueConfig::Duration { value: 1.0 },
+                },
+            ),
             (
                 "audio 1".to_string(),
                 InputParameter {
                     data_type: DataType::AudioSource,
                     kind: InputParamKind::ConnectionOnly,
-                    name: "audio source 1".to_string(),
-                    value: InputValueConfig::AudioSource {},
-                },
-            ),
-            (
-                "audio 2".to_string(),
-                InputParameter {
-                    data_type: DataType::AudioSource,
-                    kind: InputParamKind::ConnectionOnly,
-                    name: "audio source 2".to_string(),
+                    name: "audio 1".to_string(),
                     value: InputValueConfig::AudioSource {},
                 },
             ),
@@ -38,6 +39,14 @@ pub fn mix_node() -> SoundNode {
             },
         )]),
         operation: |props| {
+            let duration = props
+                .inputs
+                .get("duration")
+                .unwrap()
+                .clone()
+                .try_to_duration()
+                .unwrap();
+
             let first = props
                 .inputs
                 .get("audio 1")
@@ -45,24 +54,14 @@ pub fn mix_node() -> SoundNode {
                 .clone()
                 .try_to_source()
                 .unwrap();
-            let second = props
-                .inputs
-                .get("audio 2")
-                .unwrap()
-                .clone()
-                .try_to_source()
-                .unwrap();
 
-            let new_sound = sound_queue::push_sound(
-                (sound_queue::clone_sound(first))
-                    .mix(sound_queue::clone_sound(second))
+            let idx = sound_queue::push_sound(
+                sound_queue::clone_sound(first)
+                    .take_duration(duration)
                     .as_generic(None),
             );
 
-            HashMap::from([(
-                "out".to_string(),
-                ValueType::AudioSource { value: new_sound },
-            )])
+            HashMap::from([("out".to_string(), ValueType::AudioSource { value: idx })])
         },
     }
 }
