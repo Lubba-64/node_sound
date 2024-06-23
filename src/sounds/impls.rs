52e1d7628d93da6e910eb56fdfbbe080ba5630a6
Lubba-64 <Lubba64@gmail.com>
2024-06-23 00:03:47 -0400

    feat: Add const wave to make automated translate wave easier to use

--- src/sounds/impls.rs
+++ src/sounds/impls.rs
@@ -5,7 +5,7 @@ use super::{
 };
 use super::{
     AutomatedClamp, AutomatedMod, AutomatedSawToothWave, AutomatedSineWave, AutomatedSquareWave,
-    AutomatedTranslateWave, AutomatedTriangleWave, Signum, TranslateWave,
+    AutomatedTranslateWave, AutomatedTriangleWave, ConstWave, Signum, TranslateWave,
 };
 
 use rodio::source::{
@@ -17,11 +17,32 @@ impl RefSourceIter<f32> for SineWave {}
 impl RefSourceIter<f32> for SquareWave {}
 impl RefSourceIter<f32> for TriangleWave {}
 impl RefSourceIter<f32> for SawToothWave {}
+impl RefSourceIter<f32> for Noise {}
+impl RefSourceIter<f32> for Pop {}
+impl RefSourceIter<f32> for Zero<f32> {}
+impl RefSourceIter<f32> for ConstWave {}
 impl<I> RefSourceIter<f32> for Amplify<I> where I: RefSourceIter<f32> {}
 impl<I> RefSourceIter<f32> for BltFilter<I> where I: RefSourceIter<f32> {}
 impl<I> RefSourceIter<f32> for Delay<I> where I: RefSourceIter<f32> {}
-impl RefSourceIter<f32> for Zero<f32> {}
 impl<I> RefSourceIter<f32> for FadeIn<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for SplitChannels<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Repeat<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for SkipDuration<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Spatial<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Speed<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for TakeDuration<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for SamplesConverter<I, f32> where I: RefSourceIter<f32> {}
+impl RefSourceIter<f32> for CloneableDecoder {}
+impl<I> RefSourceIter<f32> for Clamp<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Abs<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for ChannelVolume<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Mod<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for TranslateWave<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for Signum<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for AutomatedSawToothWave<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for AutomatedSineWave<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for AutomatedSquareWave<I> where I: RefSourceIter<f32> {}
+impl<I> RefSourceIter<f32> for AutomatedTriangleWave<I> where I: RefSourceIter<f32> {}
 impl<I1, I2> RefSourceIter<f32> for Mix<I1, I2>
 where
     I1: RefSourceIter<f32>,
@@ -46,26 +67,6 @@ where
     I2: RefSourceIter<f32>,
 {
 }
-impl<I> RefSourceIter<f32> for SplitChannels<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Repeat<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for SkipDuration<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Spatial<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Speed<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for TakeDuration<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for SamplesConverter<I, f32> where I: RefSourceIter<f32> {}
-impl RefSourceIter<f32> for CloneableDecoder {}
-impl<I> RefSourceIter<f32> for Clamp<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Abs<I> where I: RefSourceIter<f32> {}
-impl RefSourceIter<f32> for Noise {}
-impl RefSourceIter<f32> for Pop {}
-impl<I> RefSourceIter<f32> for ChannelVolume<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Mod<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for TranslateWave<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for Signum<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for AutomatedSawToothWave<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for AutomatedSineWave<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for AutomatedSquareWave<I> where I: RefSourceIter<f32> {}
-impl<I> RefSourceIter<f32> for AutomatedTriangleWave<I> where I: RefSourceIter<f32> {}
 impl<I1, I2, I3> RefSourceIter<f32> for AutomatedClamp<I1, I2, I3>
 where
     I1: RefSourceIter<f32>,
