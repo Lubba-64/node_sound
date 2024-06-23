6b43b21774222057a8677d15c9d683a936f14fbd
Lubba-64 <Lubba64@gmail.com>
2024-06-23 11:43:49 -0400

    fix: Actually add event listeners

--- assets/sw.js
+++ assets/sw.js
@@ -23,3 +23,37 @@ self.addEventListener('fetch', function (e) {
     })
   );
 });
+
+self.addEventListener("touchstart", touchHandler, true);
+self.addEventListener("touchmove", touchHandler, true);
+self.addEventListener("touchend", touchHandler, true);
+self.addEventListener("touchcancel", touchHandler, true);    
+
+function touchHandler(event)
+{
+    var clickNum = event.changedTouches.length;
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;        
+        case "touchend":   type = "mouseup";   break;
+        default:           return;
+    }
+
+    // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
+    //                screenX, screenY, clientX, clientY, ctrlKey, 
+    //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                                  first.screenX, first.screenY, 
+                                  first.clientX, first.clientY, false, 
+                                  false, false, false, clickNum, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
