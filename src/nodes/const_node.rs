61c45d7fbf374e1f9792a802729ccd829acc86a8
Lubba-64 <Lubba64@gmail.com>
2024-06-23 00:06:56 -0400

    chore: remove unused import

--- src/nodes/const_node.rs
+++ src/nodes/const_node.rs
@@ -3,7 +3,7 @@ use crate::sound_graph::graph_types::{
     DataType, InputParameter, InputValueConfig, Output, ValueType,
 };
 use crate::sound_map;
-use crate::sounds::{ConstWave, Pop};
+use crate::sounds::ConstWave;
 use egui_node_graph_2::InputParamKind;
 use std::collections::BTreeMap;
 
