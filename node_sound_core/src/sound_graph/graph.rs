ec2819b0671d48804f398a08b186e88a1684e0b1
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2024-07-09 16:21:04 -0400

    feat: copy paste

--- node_sound_core/src/sound_graph/graph.rs
+++ node_sound_core/src/sound_graph/graph.rs
@@ -1,3 +1,4 @@
+use super::copy_paste::{copy_to_clipboard, paste_from_clipboard};
 use super::graph_types::InputValueConfig;
 use super::save_management::write_output_sound;
 use super::save_management::{
@@ -77,6 +78,8 @@ pub struct SoundGraphUserState {
     pub recording_length: usize,
     pub is_saved: bool,
     pub vst_output_node_id: Option<NodeId>,
+    #[serde(skip)]
+    pub is_vst: bool,
     pub is_done_showing_recording_dialogue: bool,
     #[serde(skip)]
     pub _unserializeable_state: Option<UnserializeableState>,
@@ -85,6 +88,7 @@ pub struct SoundGraphUserState {
 impl Clone for SoundGraphUserState {
     fn clone(&self) -> Self {
         Self {
+            is_vst: self.is_vst,
             active_node: self.active_node,
             active_modified: self.active_modified,
             sound_result_evaluated: self.sound_result_evaluated,
@@ -383,22 +387,25 @@ impl NodeDataTrait for NodeData {
                 user_state.active_modified = true;
             }
         }
-        if !is_recording {
-            if ui.button("⬤ Record").clicked() {
-                if user_state.active_node == ActiveNodeState::NoNode {
-                    responses.push(NodeResponse::User(ActiveNodeState::RecordingNode(node_id)));
+        if !user_state.is_vst {
+            if !is_recording {
+                if ui.button("⬤ Record").clicked() {
+                    if user_state.active_node == ActiveNodeState::NoNode {
+                        responses.push(NodeResponse::User(ActiveNodeState::RecordingNode(node_id)));
+                        user_state.active_modified = true;
+                    }
+                }
+                ui.label("Recording Duration");
+                ui.add(DragValue::new(&mut user_state.recording_length));
+            } else {
+                let button = egui::Button::new(
+                    egui::RichText::new("Recording...").color(egui::Color32::BLACK),
+                )
+                .fill(egui::Color32::GOLD);
+                if ui.add(button).clicked() {
+                    responses.push(NodeResponse::User(ActiveNodeState::NoNode));
                     user_state.active_modified = true;
                 }
-            }
-            ui.label("Recording Duration");
-            ui.add(DragValue::new(&mut user_state.recording_length));
-        } else {
-            let button =
-                egui::Button::new(egui::RichText::new("Recording...").color(egui::Color32::BLACK))
-                    .fill(egui::Color32::GOLD);
-            if ui.add(button).clicked() {
-                responses.push(NodeResponse::User(ActiveNodeState::NoNode));
-                user_state.active_modified = true;
             }
         }
 
@@ -497,157 +504,6 @@ fn _new(is_vst: bool) -> SoundNodeGraph {
     }
 }
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
-struct ClipboardData {
-    nodes: Vec<(Node<NodeData>, Pos2)>,
-    connections: Vec<(InputId, OutputId)>,
-    input_params: HashMap<InputId, InputParam<DataType, ValueType>>,
-    output_params: HashMap<OutputId, OutputParam<DataType>>,
-}
-
-fn copy_to_clipboard(state: &mut SoundGraphEditorState) {
-    let mut clipboard_data = ClipboardData {
-        connections: vec![],
-        nodes: vec![],
-        input_params: HashMap::new(),
-        output_params: HashMap::new(),
-    };
-    for node_id in state.selected_nodes.clone() {
-        let node_data = state.graph.nodes.get(node_id);
-        let node = match node_data {
-            Some(x) => x.clone(),
-            None => {
-                continue;
-            }
-        };
-        clipboard_data
-            .nodes
-            .push((node.clone(), state.node_positions[node_id].clone()));
-        for input_id in node.inputs.iter().map(|(_, id)| id) {
-            let output_id = state.graph.connections.get(*input_id);
-            match output_id {
-                Some(x) => {
-                    clipboard_data.connections.push((*input_id, *x));
-                }
-                None => {}
-            }
-        }
-        for input_id in node.inputs.iter().map(|(_, id)| id) {
-            clipboard_data.input_params.insert(
-                *input_id,
-                state.graph.inputs.get(*input_id).expect("bruh").clone(),
-            );
-        }
-        for output_id in node.outputs.iter().map(|(_, id)| id) {
-            clipboard_data.output_params.insert(
-                *output_id,
-                state.graph.outputs.get(*output_id).expect("bruh").clone(),
-            );
-        }
-    }
-
-    clipboard_data.connections = clipboard_data
-        .connections
-        .iter()
-        .unique()
-        .cloned()
-        .collect();
-
-    let mut clipboard = clippers::Clipboard::get();
-    clipboard
-        .write_text(ron::ser::to_string(&clipboard_data).expect("expect serialize to work..."))
-        .unwrap();
-}
-
-fn paste_from_clipboard(state: &mut SoundGraphEditorState, cursor_pos: Vec2) {
-    let mut clipboard = clippers::Clipboard::get();
-    let mut data: Option<ClipboardData> = None;
-    match clipboard.read() {
-        Some(clippers::ClipperData::Text(text)) => match ron::de::from_str(text.as_str()) {
-            Ok(x) => {
-                data = Some(x);
-            }
-            Err(_) => {}
-        },
-
-        Some(clippers::ClipperData::Image(_)) => {}
-
-        Some(_) => {}
-
-        None => {}
-    }
-
-    let data = match data {
-        Some(x) => x,
-        None => {
-            return;
-        }
-    };
-
-    let mut ids = vec![];
-
-    for (node, node_pos) in data.nodes.clone() {
-        let mut _id = Default::default();
-        state.graph.add_node(
-            node.user_data.name.clone(),
-            node.user_data.clone(),
-            |_graph, id| {
-                state.node_order.push(id);
-                state
-                    .node_positions
-                    .insert(id, node_pos.clone() + cursor_pos);
-                _id = id
-            },
-        );
-        ids.push(_id);
-        for input_id in node.inputs.clone() {
-            let bruh = &data.input_params[&input_id.1];
-            state.graph.add_input_param(
-                _id,
-                input_id.0,
-                bruh.typ,
-                bruh.value.clone(),
-                bruh.kind,
-                true,
-            );
-        }
-        for output_id in node.outputs.clone() {
-            let bruh = &data.output_params[&output_id.1];
-            state.graph.add_output_param(_id, output_id.0, bruh.typ);
-        }
-    }
-
-    let mut input_id_map = HashMap::new();
-    let mut output_id_map = HashMap::new();
-
-    for ((node, _node_pos), node_id) in data.nodes.iter().zip(ids.clone()) {
-        for (name, id) in state.graph.nodes[node_id].inputs.iter() {
-            for (from_name, from_id) in node.inputs.iter() {
-                if from_name == name {
-                    input_id_map.insert(*from_id, *id);
-                }
-            }
-        }
-        for (name, id) in state.graph.nodes[node_id].outputs.iter() {
-            for (from_name, from_id) in node.outputs.iter() {
-                if from_name == name {
-                    output_id_map.insert(*from_id, *id);
-                }
-            }
-        }
-    }
-
-    for (input_id, output_id) in &data.connections {
-        if input_id_map.contains_key(input_id) && output_id_map.contains_key(output_id) {
-            state
-                .graph
-                .add_connection(output_id_map[output_id], input_id_map[input_id]);
-        }
-    }
-
-    let _ = ids.iter().map(|x| state.selected_nodes.push(*x));
-}
-
 impl SoundNodeGraph {
     pub fn new(cc: Option<&eframe::CreationContext<'_>>) -> Self {
         if cc.is_some() {
@@ -656,11 +512,15 @@ impl SoundNodeGraph {
             }
         }
 
-        _new(false)
+        let mut new_ = _new(false);
+        new_.state.user_state.is_vst = new_.is_vst;
+        new_
     }
 
     pub fn new_raw() -> Self {
-        _new(true)
+        let mut new_ = _new(true);
+        new_.state.user_state.is_vst = new_.is_vst;
+        new_
     }
 
     fn save_project_file(&mut self, path: &str) {
@@ -732,6 +592,7 @@ impl SoundNodeGraph {
                     editor_state: file.1.graph_state.clone(),
                     user_state: SoundGraphUserState::default(),
                 };
+                self.state.user_state.is_vst = self.is_vst;
             }
             None => {}
         }
@@ -750,6 +611,7 @@ impl SoundNodeGraph {
             if ui.add(egui::Button::new("New Project")).clicked() {
                 self.settings_state.latest_saved_file = None;
                 self.state = SoundNodeGraphState::default();
+                self.state.user_state.is_vst = self.is_vst;
             }
             if ui.add(egui::Button::new("Save")).clicked() {
                 match self.settings_state.latest_saved_file.clone() {
@@ -813,19 +675,6 @@ impl SoundNodeGraph {
                 ui.add(egui::Label::new(crate_version!()));
                 ui.add(egui::Label::new("|"));
 
-                if ui.add(egui::Button::new("copy")).clicked() {
-                    copy_to_clipboard(&mut self.state.editor_state);
-                }
-                if ui.add(egui::Button::new("paste")).clicked() {
-                    let input =
-                        ui.input(|i| i.pointer.latest_pos().unwrap_or(Pos2 { x: 0.0, y: 0.0 }));
-                    let input_vec2 = Vec2 {
-                        x: input.x,
-                        y: input.y,
-                    };
-                    paste_from_clipboard(&mut self.state.editor_state, input_vec2);
-                }
-
                 if !self.is_vst {
                     self.combobox(ui);
                     if ui.add(egui::Link::new("tutorial")).clicked() {
@@ -851,6 +700,19 @@ impl SoundNodeGraph {
                             false => "*",
                         }
                     )));
+                }
+
+                if ui.add(egui::Button::new("copy")).clicked() {
+                    copy_to_clipboard(&mut self.state.editor_state);
+                }
+                if ui.add(egui::Button::new("paste")).clicked() {
+                    let input =
+                        ui.input(|i| i.pointer.latest_pos().unwrap_or(Pos2 { x: 0.0, y: 0.0 }));
+                    let input_vec2 = Vec2 {
+                        x: input.x,
+                        y: input.y,
+                    };
+                    paste_from_clipboard(&mut self.state.editor_state, input_vec2);
                 }
             });
         });
