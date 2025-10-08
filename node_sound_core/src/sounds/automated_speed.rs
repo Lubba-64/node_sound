bfecab7cb5ec0b5888c1ddb5d0b0773eb46c6ab1
Lubba64 <lubba64@gmail.com>
2025-10-08 11:09:32 -0400

    chore: remove unecessary logic in speed

--- node_sound_core/src/sounds/automated_speed.rs
+++ node_sound_core/src/sounds/automated_speed.rs
@@ -7,28 +7,22 @@ pub struct AutomatedSpeed<I: DawSource, I2: DawSource> {
     freq: I2,
     last_index: f32,
     adjusted_index: f32,
-    sample_rate: f32,
 }
 
 impl<I: DawSource, I2: DawSource> AutomatedSpeed<I, I2> {
-    pub fn new(source: I, base_freq: f32, freq: I2, sample_rate: f32) -> Self {
+    pub fn new(source: I, base_freq: f32, freq: I2) -> Self {
         Self {
             source,
             base_freq,
             freq,
             last_index: 0.0,
             adjusted_index: 0.0,
-            sample_rate,
         }
     }
 }
 
 impl<I: DawSource + Clone, I2: DawSource + Clone> DawSource for AutomatedSpeed<I, I2> {
-    fn next(&mut self, mut index: f32, channel: u8) -> Option<f32> {
-        index %= self.sample_rate;
-        if self.last_index > index {
-            self.last_index = 0.0;
-        }
+    fn next(&mut self, index: f32, channel: u8) -> Option<f32> {
         self.adjusted_index += (index - self.last_index)
             * (self.freq.next(index, channel).unwrap_or(0.0) / self.base_freq);
         self.last_index = index;
